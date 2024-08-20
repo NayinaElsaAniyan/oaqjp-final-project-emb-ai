39809dc6b635dbc81e7e9d03f8e2a5e227c29ede
NayinaElsaAniyan <nayina.elsa.aniyan@gmail.com>
2024-08-20 07:56:33 -0400

    changes made

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,28 +1,27 @@
 import requests  # Import the requests library to handle HTTP requests
 import json
 
-def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
-    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
-    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+def emotion_detector(text_to_analyse):  
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  
+    myobj = { "raw_document": { "text": text_to_analyse } }  
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  
+    response = requests.post(url, json=myobj, headers=header)  
     
     formatted_response = json.loads(response.text)
-    # Extracting sentiment label and score from the response
-    emotion_data = formatted_response['emotionPredictions'][0]['emotion']
     
-    # Get scores for each emotion
-    anger_score = emotion_data['anger']
-    disgust_score = emotion_data['disgust']
-    fear_score = emotion_data['fear']
-    joy_score = emotion_data['joy']
-    sadness_score = emotion_data['sadness']
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    
     dominant_emotion = max(
         [('anger', anger_score), ('disgust', disgust_score), ('fear', fear_score),
          ('joy', joy_score), ('sadness', sadness_score)],
         key=lambda x: x[1]
-    )[0] 
-    result = {
+    )[0]
+    
+    return {
         'anger': anger_score,
         'disgust': disgust_score,
         'fear': fear_score,
@@ -30,8 +29,3 @@ def emotion_detector(text_to_analyse):  # Define a function named sentiment_anal
         'sadness': sadness_score,
         'dominant_emotion': dominant_emotion
     }
-    
-    return "\n".join(f"{key}: {value}" for key, value in result.items())
-        
-   
-    #return response.text  # Return the response text from the API
